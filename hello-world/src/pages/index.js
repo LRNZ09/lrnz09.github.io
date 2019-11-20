d9638191f98a9e30a776b26b0848d279c1f888b8
Lorenzo Pieri <lorenzo196pieri@gmail.com>
2019-11-20 10:24:48 +0100

    Create project with gatsby

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => (
+    <div>
+        <h1>Work in progress...</h1>
+        <p>Come back soon!</p>
+    </div>
+)
+
+export default Home
---
+++ hello-world/src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from 'typography'
+import fairyGatesTheme from 'typography-theme-fairy-gates'
+
+const typography = new Typography(fairyGatesTheme)
+
+export default typography
