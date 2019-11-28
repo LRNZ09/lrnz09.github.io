c8c8748480aa8e7955ae3d03cd6c2677462c50cd
Lorenzo Pieri <lorenzo196pieri@gmail.com>
2019-11-28 19:11:06 +0100

    Fix columns spacing and add some more social links

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -7,11 +7,11 @@ import theme from '../utils/theme'
 
 const Home = () => (
     <Box row justifyContent="center">
-        <Box col={{ xs: 1, md: 2 / 3 }}>
+        <Box col={{ xs: 1, md: 2 / 3 }} p={24}>
             <Text as="h1">Hello</Text>
 
-            <Box row>
-                <Box col>
+            <Box row m={-24}>
+                <Box col={{ xs: 1, md: 1 / 2 }} p={24} m={0}>
                     <Text as="h2">Bio</Text>
 
                     <Text>
@@ -29,28 +29,33 @@ const Home = () => (
                     </Text>
                 </Box>
 
-                <Box col>
+                <Box col={{ xs: 1, md: 1 / 2 }} p={24} m={0}>
                     <Text as="h2">Contact me</Text>
-
                     <Text>
                         Reasons to get in touch: discuss about crazy ideas, just
                         meet, asking for advice.
                     </Text>
-
                     <Text as="h4">Mail</Text>
                     <Text as="a" href="mailto:hello@lorenzopieri.dev">
                         hello@lorenzopieri.dev
                     </Text>
-
                     <Text as="h4">Social</Text>
+                    <Text as="a" href="//dribbble.com/LRNZ09" target="_blank">
+                        Dribbble
+                    </Text>{' '}
                     <Text
                         as="a"
                         href="//linkedin.com/in/lrnz09"
                         target="_blank"
                     >
                         Linkedin
+                    </Text>{' '}
+                    <Text as="a" href="//medium.com/@LRNZ09" target="_blank">
+                        Medium
+                    </Text>{' '}
+                    <Text as="a" href="//twitter.com/LRNZ09" target="_blank">
+                        Twitter
                     </Text>
-                    {/* TODO */}
                 </Box>
             </Box>
         </Box>
