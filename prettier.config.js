f15f946751af01177e88404ae8d21398583b3648
Lorenzo Pieri <email@lorenzopieri.dev>
2020-04-12 19:00:36 +0200

    Upgrade packages

--- prettier.config.js
+++ prettier.config.js
@@ -1,21 +1,9 @@
 /** @format */
 
 module.exports = {
-	arrowParens: 'always',
-	bracketSpacing: true,
-	endOfLine: 'auto',
-	htmlWhitespaceSensitivity: 'ignore',
 	insertPragma: true,
-	jsxBracketSameLine: false,
-	jsxSingleQuote: false,
-	printWidth: 79,
-	proseWrap: 'preserve',
-	quoteProps: 'as-needed',
-	requirePragma: false,
+	jsxSingleQuote: true,
 	semi: false,
 	singleQuote: true,
-	tabWidth: 4,
-	trailingComma: 'all',
-	useTabs: true,
-	vueIndentScriptAndStyle: false,
+	useTabs: true
 }
