504c709a65ccdd15c388ceac43859cc432f10f3c
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-24 08:31:40 +0100

    chore: add and configure biome

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Checked with Biome](https://img.shields.io/badge/Checked_with-Biome-60a5fa?style=flat&logo=biome)](https://biomejs.dev)
+
 # `create-preact`
 
 <h2 align="center">
--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,25 @@ import './style.css';
 export function App() {
 	return (
 		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
+			<a href='https://preactjs.com' target='_blank' rel='noreferrer'>
+				<img src={preactLogo} alt='Preact logo' height='160' width='160' />
 			</a>
 			<h1>Get Started building Vite-powered Preact Apps </h1>
 			<section>
 				<Resource
-					title="Learn Preact"
+					title='Learn Preact'
 					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
+					href='https://preactjs.com/tutorial'
 				/>
 				<Resource
-					title="Differences to React"
+					title='Differences to React'
 					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
+					href='https://preactjs.com/guide/v10/differences-to-react'
 				/>
 				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
+					title='Learn Vite'
+					description='To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation'
+					href='https://vitejs.dev'
 				/>
 			</section>
 		</div>
@@ -33,7 +33,7 @@ export function App() {
 
 function Resource(props) {
 	return (
-		<a href={props.href} target="_blank" class="resource">
+		<a href={props.href} target='_blank' class='resource' rel='noreferrer'>
 			<h2>{props.title}</h2>
 			<p>{props.description}</p>
 		</a>
