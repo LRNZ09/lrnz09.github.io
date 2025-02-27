32e25cfeb71e51945c7a0ef69cc7e55444d109a2
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-28 00:00:40 +0100

    feat: add work in progress modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,20 @@
-import { List, TaskBar } from '@react95/core';
-import { Progman8 } from '@react95/icons';
+import { List, Modal, TaskBar } from '@react95/core';
+import { InfoBubble, Progman8 } from '@react95/icons';
 
 export const App = () => {
 	return (
-		<TaskBar
-			list={
-				<List>
-					<List.Item icon={<Progman8 />}>Hang in there</List.Item>
-				</List>
-			}
-		/>
+		<>
+			<Modal icon={<InfoBubble />} left='50px' title='About'>
+				<Modal.Content>Work in progress...</Modal.Content>
+			</Modal>
+
+			<TaskBar
+				list={
+					<List>
+						<List.Item icon={<Progman8 />}>Hang in there</List.Item>
+					</List>
+				}
+			/>
+		</>
 	);
 };
